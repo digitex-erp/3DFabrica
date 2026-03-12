f8b529ff3de2cffee65380ff42efc60b6c3dd4ac
Digitex Studio <digitex.studio@gmail.com>
2026-03-12 12:30:13 +0530

    Fix: Remove Auth0 discrepancy, add runtime diagnostics, MVP ready for beta

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { Auth0Provider } from "@auth0/auth0-react";
-import { auth0Config } from "./lib/auth";
 
 import { TempoDevtools } from "tempo-devtools";
 TempoDevtools.init();
@@ -18,10 +16,8 @@ const basename = import.meta.env.BASE_URL;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Auth0Provider {...auth0Config}>
-      <BrowserRouter basename={basename}>
-        <App />
-      </BrowserRouter>
-    </Auth0Provider>
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
